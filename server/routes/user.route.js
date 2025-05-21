88f185eddb24dfcadcf85433266f8264ca3a1c38
AdhithyanAsus <adhithyanktd@gmail.com>
2025-05-21 14:05:15 +0530

    get product by id route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { getAllProducts } from "../controllers/user.controller.js";
+import { getProduct } from "../controllers/user.controller.js";
 
 const router = express();
 
 router.get("/all-products", getAllProducts);
+
+router.get("/product/:id", getProduct);
 
 export default router;
