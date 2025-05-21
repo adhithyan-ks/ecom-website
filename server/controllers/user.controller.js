7b1419d93f41cdf6f9d40669973278e1fd2b35fa
AdhithyanAsus <adhithyanktd@gmail.com>
2025-05-21 13:59:41 +0530

    get product

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,13 +1,28 @@
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async(req, res) => {
-    
     try {
         const allProducts = await Product.find();
         if(!allProducts) {
             return res.json({success: false, message: "No products found!"});
         }
         res.json({success: true, message: "Products found", total: allProducts.length, products: allProducts});
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+export const getProduct = async(req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.json({success: false, message: "Product Id is required!"});
+    }
+    try {
+        const product = await Product.findById(id);
+        if(!product) {
+            return res.json({success: false, message: "Product was not foound!"});
+        }
+        return res.json({success: true, message: "Product found", product: product});
     }
     catch (error) {
         console.log(error);
