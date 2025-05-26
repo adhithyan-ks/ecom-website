cdb9b7ebe3d11fe75943b571ab0570e713bb4ac6
AdhithyanAsus <adhithyanktd@gmail.com>
2025-05-26 12:47:41 +0530

    Get cart and add to cart functions and routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import Cart from "../models/cart.model.js";
 
 export const getAllProducts = async(req, res) => {
     try {
@@ -26,5 +27,62 @@ export const getProduct = async(req, res) => {
     }
     catch (error) {
         console.log(error);
+    }
+}
+
+//Cart
+export const addToCart = async(req, res) => {
+    try {
+        // const userId = req.user.id;
+        const userId = "682ad879c70e706186bbf568";
+
+        const { productId, quantity } = req.body;
+        const product =  await Product.findById(productId);
+        if (!product) {
+            return res.status(404).json({ success: false, message: "Product was not found! "});
+        }
+
+        //Find or create cart for the user
+        let cart = await Cart.findOne({ userId });
+
+        if (!cart) {
+            //Creates new cart if none exists
+            cart = new Cart({
+                userId,
+                products: [{ product: productId, quantity }],
+                totalAmount: product.price * quantity
+            });
+        } else {
+            const existingProduct = cart.products.find(item => item.product.toString() === productId);
+        
+            if (existingProduct) {
+                existingProduct.quantity += quantity;
+            } else {
+                cart.products.unshift({ product: productId, quantity});
+            }
+            cart.totalAmount = cart.products.reduce((total, item) => {
+                return total + (item.product.toString() === productId ? product.price : 0) * item.quantity;
+            }, 0);
+        }
+        await cart.save();
+        res.status(200).json({ success: true, message: "Product added to cart succcessfully", cart});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+//Get cart
+export const getCart = async(req, res) => {
+    try {
+        // const userId = req.user.id;
+        const userId = "682ad879c70e706186bbf568";
+        let cart = await Cart.findOne({ userId });
+        if(!cart) {
+            return res.json({ success:true, message: "Cart is empty" });
+        }
+        return res.status(200).json({ success: true, cart});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllProducts } from "../controllers/user.controller.js";
-import { getProduct } from "../controllers/user.controller.js";
+import { getAllProducts, getProduct, addToCart, getCart } from "../controllers/user.controller.js";
 
 const router = express();
 
 router.get("/all-products", getAllProducts);
 router.get("/product/:id", getProduct);
+
+router.post("/add-to-cart", addToCart);
+router.get("/cart", getCart);
 
 export default router;
