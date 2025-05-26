955b8391b73390334f17a6e5748b9efa1957157c
AdhithyanAsus <adhithyanktd@gmail.com>
2025-05-26 15:38:03 +0530

    demo code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="text-4-xl text-black font-bold p-4">
+      <h1 className="text-1-xl text-red-500">Sign Up</h1>
+      <label className="block mb-2">Email:</label>
+      <input type="email" className="border border-gray-300 p-2 mb-4" placeholder="Enter your email" />
+      <label className="block mb-2">Password:</label>
+      <input type="password" className="border border-gray-300 p-2 mb-4" placeholder="Enter your password" />
+      <button className="bg-blue-500 text-white p-2 block rounded">Sign Up</button>
+    </div>
   )
 }
-
-export default App
+export default App;
