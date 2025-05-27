6df66c1bdd93cb2ab57d61ab18fbbaa4676ecf04
AdhithyanAsus <adhithyanktd@gmail.com>
2025-05-27 12:09:38 +0530

    Sign up route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="text-4-xl text-black font-bold p-4">
-      <h1 className="text-1-xl text-red-500">Sign Up</h1>
-      <label className="block mb-2">Email:</label>
-      <input type="email" className="border border-gray-300 p-2 mb-4" placeholder="Enter your email" />
-      <label className="block mb-2">Password:</label>
-      <input type="password" className="border border-gray-300 p-2 mb-4" placeholder="Enter your password" />
-      <button className="bg-blue-500 text-white p-2 block rounded">Sign Up</button>
-    </div>
+    <UserProvideer>
+      <Routes>
+        <Route path="/sign-up" element={<SignUpPage/>}></Route>
+      </Routes>
+    </UserProvideer>
   )
 }
 export default App;
