988553b83c5ea510a593ab6a6f5a6b27987d7cc9
adhithyan-ks <adhithyanktd@gmail.com>
2025-05-31 10:11:04 +0530

    Cart page started

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
