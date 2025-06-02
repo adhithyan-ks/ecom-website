e6be70ccbaabeb7f9a9f59eff5ba08bcff317a40
adhithyan-ks <adhithyanktd@gmail.com>
2025-06-02 12:32:37 +0530

    Sign in fix

--- server/lib/verifyUser.js
+++ server/lib/verifyUser.js
@@ -2,8 +2,16 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookie.access_token;
+    const token = req.cookies.access_token;
     if (!token) {
         return next(errorHandler(401, "Unauthorized. Sign-in to continue."));
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        console.log("Decoded user:", decoded);
+        next();
+    } catch (error) {
+        console.log(error);
     }
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getAllProducts, getProduct, addToCart, getCart } from "../controllers/user.controller.js";
+import { verifyToken } from "../lib/verifyUser.js";
 
 const router = express();
 
 router.get("/all-products", getAllProducts);
 router.get("/product/:id", getProduct);
 
-router.post("/add-to-cart", addToCart);
-router.get("/cart", getCart);
+router.post("/add-to-cart", verifyToken, addToCart);
+router.get("/cart", verifyToken, getCart);
 
 export default router;
