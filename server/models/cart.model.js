516e76c64c4cde8048877fc9c889f4dab990ed65
adhithyan-ks <adhithyanktd@gmail.com>
2025-06-02 12:32:59 +0530

    removed unnecessary comments

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -6,27 +6,17 @@ const CartSchema = new mongoose.Schema({
         ref: "User",
         required: true
     },
-//     products: [{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "Products",
-//         quantity: {
-//             type: Number,
-//             default: 1
-//         }
-//     }],
-// --- CORRECTED PRODUCTS ARRAY DEFINITION ---
-        products: [{
-            product: { // This defines an embedded object for each item
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: true // It's good practice to make the product ID required
-            },
-            quantity: {
-                type: Number,
-                default: 1
-            }
-        }],
-// ------------------------------------------
+    products: [{
+        product: { // This defines an embedded object for each item
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true // It's good practice to make the product ID required
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        }
+    }],
     totalAmount: {
         type: Number,
         default: 0
